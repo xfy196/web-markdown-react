ad087e640767ab7be29a3f78ac12001532e3c77a
xfy196 <xufuying@outlook.com>
2021-06-02 11:24:51 +0800

    在线markdown版本发布

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MarkdownEditor from "./md-react/src/index.ts"
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <MarkdownEditor></MarkdownEditor>
+    </div>
+  );
+}
+
+export default App;
